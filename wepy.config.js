758b32c56009e851d4627adc64511d76a9d6cec5
xxz <1316143408@qq.com>
2020-06-08 10:29:04 +0800

    init

---
+++ wepy.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+var prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: !prod,
+  static: ['static'],
+  build: {
+  },
+  resolve: {
+    alias: {
+      counter: path.join(__dirname, 'src/components/counter'),
+      '@': path.join(__dirname, 'src'),
+      '@vant':path.join(__dirname,'src/components/van/dist')
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    less: {
+      compress: prod
+    },
+    babel: {
+      sourceMap: true,
+      presets: [
+        '@babel/preset-env'
+      ],
+      plugins: [
+        '@wepy/babel-plugin-import-regenerator'
+      ]
+    }
+  },
+  plugins: [],
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
